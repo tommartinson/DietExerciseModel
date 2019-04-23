466527d6ef393802d9e60f9b0511c2ea96a71ec6
Tom Martinson <tomemartinson@gmail.com>
2019-04-23 14:31:18 -0700

    changed input

--- DietExerciseModel.py
+++ DietExerciseModel.py
@@ -7,24 +7,22 @@
 # import numpy as np
 import matplotlib.pyplot as plot
 
-
-
 timepoints = []
 xpoints = []
 ypoints = []
 
 
-alpha = 0.75
-beta = 0.85
+rate = 0.5
 
-dt = 0.01 # timestep
+
+dt = 1 # timestep
 
 # initialization
 time = 0
 x = 10000
-y = 10000
+
 dx = 0
-dy = 0
+
 
 age = input("Please enter your age:")
 gender = input("Please enter your gender (M,F):")
@@ -35,36 +33,34 @@ exerciseLevel = input("Please enter the level of exercise you would like to mode
 
 deficitPercent = input("Please enter the % caloric deficit you would like to model (1-4)\n1. 5% below maintenance level\n2. 10% below maintenance level\n3. 15% below maintenance level\n4. 20% below maintenance level \n:")
 
+totalTime = 30 * int(input("How many months would you like to model this diet for?"))
+
 # print output header and starting values
 print ("  Time  Weight   Change")
-print ("%6.2f  %6.2f%6.2f " % (time, x, dx*dt))
+print ("%6.2f  %6.2f%6.2f " % (time, x, dx))
 
 
 # integration time loop
-while x > 0 and y > 0 : # run until a troop is eliminated
+while time < totalTime: # run until a troop is eliminated
   # compute derivatives
-  dx = -beta*y  # x attrition rate
-  dy = -alpha*x  # y attrition rate
+  dx = -rate  # x attrition rate
+  
   # increment time
   time += dt # time = time+ dt
   # update troop state variables
-  x += dx*dt # x = x + dx*dt
+  x += dx # x = x + dx*dt
   if(x<0):
     x = 0
-  y += dy*dt # y = y + dy*dt
-  if(y<0):
-    y = 0
+  
   
   print ("%6.2f  %6.2f  %6.2f " % (time, x, dx*dt))
   xpoints.append(x)
-  ypoints.append(y)
   timepoints.append(time)
   
  
 plot.xlabel('Time (days)')
 plot.ylabel('Weight (lb)')
 plot.plot(timepoints,xpoints)
-plot.plot(timepoints,ypoints)
 plot.legend(loc='upper center')
 plot.show()
 plot.savefig('output.png')
